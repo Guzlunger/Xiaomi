e8a061f2926e4f30c7696368ee6eb554daeb7496
Guzlunger <sh.rabadanov23@gmail.com>
2022-06-09 22:54:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+  $('.bike__slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay:true,
+    autoplaySpeed: 2000
+  });
+})
+
+$(function(){
+  $('.second-bike-slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    variableWidth: true
+  });
+})
